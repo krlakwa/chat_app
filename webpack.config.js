438341f6b85f0e39b4a05405c971412e72e7cc1a
Krzysztof Łakwa <fenrir14@gmail.com>
2018-06-21 10:31:16 +0200

    Update config

--- webpack.config.js
+++ webpack.config.js
@@ -1,61 +1,78 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const OptimizeJsPlugin = require('optimize-js-plugin');
+const webpack = require("webpack");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const UglifyJSPlugin = require("uglifyjs-webpack-plugin");
+const OptimizeJsPlugin = require("optimize-js-plugin");
 
-/*const plugins = [new HtmlWebpackPlugin({
-    template: 'src/index.html',
-    filename: 'index.html',
-    inject: 'body'
-})]*/
+var env = (process.env.NODE_ENV || "development").trim();
 
-module.exports = (env) => {
-    if (env === 'production') {
-        plugins.push(
-            new OptimizeJsPlugin({
-                sourceMap: false
-            })
-        )
-    }
-    const environment = env || 'production';
+console.log("NODE_ENV:", env);
 
-    return {
-        mode: environment,
-        entry: (env !== 'production' ? [
-            'react-hot-loader/patch',
-            'webpack-dev-server/client?http://localhost:8080',
-            'webpack/hot/only-dev-server',
-        ] : []).concat(['./client/index.js']),
-        output: {
-        filename: './bundle.js',
-        path: path.resolve(__dirname, 'public')
-    },
-        module: {
-            rules: [
-                {
-                    test: /\.js$/,
-                    exclude: /node_modules/,
-                    loader: "babel-loader",
-                    options: {
-                        plugins: env !== 'production' ? ["react-hot-loader/babel"] : []
-                    }
-                },
-                {
-                    test: /\.css$/,
-                    use: [
-                        {
-                            loader: 'style-loader'
-                        },
-                        {
-                            loader: 'css-loader',
-                            options: {
-                                modules: true
-                            }
-                        }
-                    ]
-                }
-            ]
-        }
-        //plugins
-    }
-    
+function getPlugins() {
+	var plugins = [
+		new HtmlWebpackPlugin({
+			template: "client/index.html",
+			filename: "index.html",
+			inject: "body"
+		})
+	];
+	if (env === "production") {
+		console.log("prod build");
+		plugins.push(
+			new webpack.optimize.UglifyJsPlugin(),
+			new OptimizeJsPlugin({
+				sourceMap: false
+			})
+		);
+	} else {
+    plugins.push(new webpack.HotModuleReplacementPlugin());
+  }
+
+	return plugins;
 }
+
+module.exports = {
+	entry: (env !== "production"
+		? [
+				"react-hot-loader/patch",
+				"webpack-dev-server/client?http://localhost:8080",
+				"webpack/hot/only-dev-server"
+		  ]
+		: []
+	).concat(["./client/index.js"]),
+	output: {
+		filename: "./bundle.js",
+		path: path.resolve(__dirname, "public")
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: "babel-loader"
+			},
+			{
+				test: /\.css$/,
+				use: [
+					{ loader: "style-loader" },
+					{
+						loader: "css-loader",
+						options: {
+							modules: true
+						}
+					}
+				]
+			}
+		]
+	},
+  devtool: 'cheap-module-eval-source-map',
+  devServer: {
+    proxy: {
+      '/socket.io': {
+        target: 'http://localhost:3000',
+        ws: true,
+      },
+    },
+  },
+	plugins: getPlugins()
+};
